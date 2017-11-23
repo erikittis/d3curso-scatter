08a183a34830fbac2e9dcf952d4c19a8fd0aa46e
Erikis <erika.alvarez@tadaima.studio>
2017-11-23 21:50:46 +0100

    Initial commit

---
+++ scatterplot.js
@@ -0,0 +1,35 @@
+console.log("hola")
+
+d3.json("partidos.js", function(data) {
+    
+    console.log("wiii")
+    
+    window.data = data
+    
+    partidos = data.partidos
+    
+    ancho = 500
+    alto = 500
+    
+    escalacx = d3.scaleLinear()
+        .domain(["0", "10"])
+        .range(["0", "500"])
+    
+    contenedor= d3.select("#contenedor")
+        .append("svg")
+        .attr("width", ancho)
+        .attr("height", alto)
+    
+    
+    contenedor
+        .selectAll("circle")
+        .data(partidos)
+        .enter()
+        .append("circle")
+        .attr("cx", function(d) {return escalacx(
+        d.mediaAutoubicacion)})
+        .attr("r", 10)
+        .attr("cy", 10)
+    
+    
+})
